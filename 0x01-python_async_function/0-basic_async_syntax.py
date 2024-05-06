92692737d7b5654c25305e32d5cb58e1d5298b29
zakariasan <elitziko@gmail.com>
2024-05-07 00:55:23 +0100

    basic_ async syntax

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """type-annotated function add that takes a float a and
 a float b as arguments and returns their sum as a float."""
+
 import asyncio
 import random
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay: int = 10) -> float:
     """Return playing with random"""
     time = random.uniform(0, max_delay)
     await asyncio.sleep(time)
