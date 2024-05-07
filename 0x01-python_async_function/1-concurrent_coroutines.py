45e1fcbd8366667d5650063c7ef769cb9363c562
zakariasan <elitziko@gmail.com>
2024-05-07 01:05:22 +0100

    concurrent coroutines

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -3,10 +3,19 @@
 a float b as arguments and returns their sum as a float."""
 import asyncio
 from typing import List
-from basic_async_syntax import wait_random
+wait_r = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int = 10) -> List[float]:
-    """Waits for random delay up to max_delay and returns list of actual"""
-    de = await asyncio.gather(*(wait_random(max_delay) for _ in range(n)))
-    return de
+async def wait_n(num_tasks: int, max_delay: int = 10) -> List[float]:
+    """Waits for a random delay up to max_delay and returns a list """
+    tasks = []
+    delays = []
+
+    for _ in range(num_tasks):
+        task = asyncio.create_task(wait_r(max_delay))
+        task.add_done_callback(lambda future: delays.append(future.result()))
+        tasks.append(task)
+
+    await asyncio.gather(*tasks)
+
+    return delays
