521d538e525d3e848a279d2858b7428fae390168
zakariasan <elitziko@gmail.com>
2024-05-08 02:14:05 +0100

    python async comprehension

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,9 +4,10 @@ a float b as arguments and returns their sum as a float."""
 
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """ try to asyc generator """
     for _ in range(10):
         await asyncio.sleep(1)
