69326ea68451828224580bc079b1958b51ccbab0
zakariasan <elitziko@gmail.com>
2024-06-08 05:51:43 +0100

    wait randoms

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -7,16 +7,13 @@ import random
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(num_tasks: int, max_delay: int = 10) -> List[float]:
+async def wait_n(n: int, max_delay: int = 10) -> List[float]:
     """Waits for a random delay up to max_delay and returns a list """
-    tasks = []
     delays = []
 
-    for _ in range(num_tasks):
-        task = asyncio.create_task(wait_random(max_delay))
-        task.add_done_callback(lambda future: delays.append(future.result()))
-        tasks.append(task)
-
-    await asyncio.gather(*tasks)
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    for task in asyncio.as_completed(tasks):
+        delay = await task
+        delays.append(delay)
 
     return delays
