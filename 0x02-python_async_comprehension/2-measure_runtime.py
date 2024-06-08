be6a2e6e7180cc5df3a7f8e0e9e8febc93235588
zakariasan <elitziko@gmail.com>
2024-06-08 06:42:53 +0100

    wait randoms

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -11,10 +11,8 @@ async def measure_runtime() -> float:
     """measure_runtime should measure the total runtime and return it.
 Notice that the total runtime is roughly 10 seconds, explain it to yourself."""
     start = time.time()
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension()
-    )
+    await asyncio.gather(async_comprehension(), async_comprehension(),
+                         async_comprehension(),
+                         async_comprehension()
+                         )
     return time.time() - start
